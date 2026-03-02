05d7aeb4b4860c71ebf85cc6235b589266f40547
adiasdjas <aymensdhuasdjo@gmail.com>
2026-03-02 15:55:33 +0300

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -109,7 +109,7 @@ getgenv().ExunysDeveloperAimbot = {
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
 		LockPart = "Head", -- Body part to lock on
 
-		TriggerKey = Enum.KeyCode.RightAlt,
+		TriggerKey = Enum.KeyCode.LeftAlt,
 		Toggle = false
 	},
 
